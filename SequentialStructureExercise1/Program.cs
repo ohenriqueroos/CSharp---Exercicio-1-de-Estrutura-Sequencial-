deb31626662725a7b2973a270869407a5280194b
henri <henri@HENRIQUE>
2024-04-18 23:03:48 -0300

    Add project files.

---
+++ SequentialStructureExercise1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SequentialStructureExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+
+            int result = num1 + num2;
+
+            Console.WriteLine("SOMA = " + result);
+        }
+    }
+}
